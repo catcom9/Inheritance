3aad56465a45b62c7f9f5d8a96a2e68d78bde173
Cathal Commins <g00427615@ATU.IE>
2024-11-18 09:33:38 +0000

    Added persons and Customers class

--- src/main/java/ie/atu/week9/Main.java
+++ src/main/java/ie/atu/week9/Main.java
@@ -1,7 +1,28 @@
 package ie.atu.week9;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String name = input.nextLine();
+
+        System.out.println("Please enter your address");
+        String address = input.nextLine();
+
+        System.out.println("Please enter your phone");
+        String phone = input.nextLine();
+
+        System.out.println("Please enter your customer number");
+        String custNum = input.nextLine();
+
+        System.out.println("Do you want to be on the mailing list");
+        boolean list = input.hasNextBoolean();
+
+        Customer newCust = new Customer(name, address, phone, custNum, list);
+        System.out.println("You entered " + newCust.toString());
+
 
     }
 }
--- src/main/java/ie/atu/week9/Person.java
+++ src/main/java/ie/atu/week9/Person.java
@@ -1,5 +1,30 @@
 package ie.atu.week9;
 
 public class Person {
-    private
+    private String name;
+    private String address;
+    private String phone;
+
+    public Person() {
+        this.name = null;
+        this.address = null;
+        this.phone = null;
+    }
+
+
+    public Person(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' ;
+    }
+
+
+
 }
